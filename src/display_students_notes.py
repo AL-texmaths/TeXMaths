1a4d2086d191beea57d4622fe8621d869100c189
AL-TeXMaths <Antoine.Longelin@ac-versailles.fr>
2026-07-11 22:43:35 +0200

    display students list tasks

--- src/display_students_notes.py
+++ src/display_students_notes.py
@@ -3,31 +3,35 @@ import json
 from tabulate import tabulate
 from src.tools import get_config, resolve_pathes
 
-classe_key = sys.argv[1].split(',') if len(sys.argv) > 1 else input("Entrez le nom de la classe : ")
-
+classes = sys.argv[1].split(",")
 config  = get_config()
 STUDENT_LISTS_DIR = resolve_pathes(*config["paths"]["student_lists"])
 LIST_KEY = config["settings"]["student_lists"]["list_key"]
 
+for classe_key in classes:
+    list_path = STUDENT_LISTS_DIR / LIST_KEY.format(classe_key, "json")
 
-with open(STUDENT_LISTS_DIR / LIST_KEY.format(classe_key, "json"), encoding="utf-8") as f:
-    data = json.load(f)
+    if not list_path.exists():
+        print(f"La classe {classe_key} n'existe pas.")
+        continue
+    with open(list_path, encoding="utf-8") as f:
+        data = json.load(f)
 
-table = []
+    table = []
 
-for eleve in data.values():
-    notes = [p["note"] for p in eleve["passages"]]
+    for eleve in data.values():
+        notes = [p["note"] for p in eleve["passages"]]
 
-    moyenne = sum(notes) / len(notes) if notes else None
+        moyenne = sum(notes) / len(notes) if notes else None
 
-    table.append([
-        eleve["name"],
-        len(notes),
-        f"{moyenne:.2f}" if moyenne is not None else "-"
-    ])
+        table.append([
+            eleve["name"],
+            len(notes),
+            f"{moyenne:.2f}" if moyenne is not None else "-"
+        ])
 
-print(tabulate(
-    table,
-    headers=["Élève", "Nb passages", "Moyenne"],
-    tablefmt="rounded_grid"
-))
+    print(tabulate(
+        table,
+        headers=["Élève", "Nb passages", "Moyenne"],
+        tablefmt="rounded_grid"
+    ))
