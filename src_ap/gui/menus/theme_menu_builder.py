3b50c037eef9a9b4eec1d5bc2392bb0e68a0173a
Antoine Longelin <Antoine.Longelin@ac-versailles.fr>
2026-07-12 10:32:58 +0200

    BUG FIX: theme preview callied multiple times

--- src_ap/gui/menus/theme_menu_builder.py
+++ src_ap/gui/menus/theme_menu_builder.py
@@ -7,7 +7,8 @@ class ThemeMenuBuilder:
     def __init__(self, theme_service, action_method, preview_method):
         self.theme_service = theme_service
         self.action_method = action_method
-        self.preview_method = preview_method
+        self._preview_method = preview_method
+        self.current_theme_name = None
         self._hover_timer = QTimer()
         self._hover_timer.setSingleShot(True)
         self._hover_timer.timeout.connect(self._apply_preview)
@@ -17,6 +18,11 @@ class ThemeMenuBuilder:
         """Apply the pending preview after debounce delay."""
         if self._pending_theme is not None:
             self.preview_method(self._pending_theme)
+    
+    def preview_method(self, name):
+        if name != self.current_theme_name:
+            self.current_theme_name = name
+            self._preview_method(name)
 
     def populate(self, menu):
 
@@ -33,9 +39,9 @@ class ThemeMenuBuilder:
             menu.addAction(action)
             action.hovered.connect(
                 lambda name=theme_name:
-                    self._schedule_preview(name)
+                    self.preview_method(name)
             )
-
+    
     def _schedule_preview(self, theme_name):
         """Schedule a preview with debouncing to prevent multiple rapid calls."""
         self._pending_theme = theme_name
